ecb4b57c5f273a74bd15f0b77645187a8ad98f75
Wyatt Brendle <accounts@wyattbrendle.tech>
2022-11-30 18:37:55 -0500

    added addition, multiplication, subtraction, and division functionality to the calculator

--- scripts.js
+++ scripts.js
@@ -1,12 +1,17 @@
 //initialize variables
-let toShow = "0";
+let initial = 0;
 let initialized = false;
-let firstNumber = toShow;
-let secondNumber = toShow;
+let firstNumber = 0;
+let secondNumber = 0;
+let operatorInitialized = false;
+let addition = false;
+let subtraction = false;
+let multiplication = false;
+let division = false;
 
 //display
 const display = document.getElementById("display");
-display.innerText = toShow;
+display.innerHTML = initial;
 
 //number buttons
 const numberOne = document.getElementById("one");
@@ -33,3 +38,190 @@ backButton.textContent = "<--";
 
 //equals button
 const equalButton= document.getElementById("equal");
+
+//add event liseners to add to toShow string
+numberOne.addEventListener("click", () => {
+    if (!initialized) {
+        display.innerText = "1";
+        initialized = true;
+    } else {
+        display.innerText += "1";
+    }
+});
+
+numberTwo.addEventListener("click", () => {
+    if (!initialized) {
+        display.innerText = "2";
+        initialized = true;
+    } else {
+        display.innerText += "2";
+    }
+});
+
+numberThree.addEventListener("click", () => {
+    if (!initialized) {
+        display.innerText = "3";
+        initialized = true;
+    } else {
+        display.innerText += "3";
+    }ne
+});
+
+numberFour.addEventListener("click", () => {
+    if (!initialized) {
+        display.innerText = "4";
+        initialized = true;
+    } else {
+        display.innerText += "4";
+    }
+});
+
+numberFive.addEventListener("click", () => {
+    if (!initialized) {
+        display.innerText = "5";
+        initialized = true;
+    } else {
+        display.innerText += "5";
+    }
+});
+
+numberSix.addEventListener("click", () => {
+    if (!initialized) {
+        display.innerText = "6";
+        initialized = true;
+    } else {
+        display.innerText += "6";
+    }
+});
+
+numberSeven.addEventListener("click", () => {
+    if (!initialized) {
+        display.innerText = "7";
+        initialized = true;
+    } else {
+        display.innerText += "7";
+    }
+});
+
+numberEight.addEventListener("click", () => {
+    if (!initialized) {
+        display.innerText = "8";
+        initialized = true;
+    } else {
+        display.innerText += "8";
+    }
+});
+
+numberNine.addEventListener("click", () => {
+    if (!initialized) {
+        display.innerText = "9";
+        initialized = true;
+    } else {
+        display.innerText += "9";
+    }
+});
+
+numberZero.addEventListener("click", () => {
+    if (!initialized) {
+        display.innerText = "0";
+        initialized = true;
+    } else {
+        display.innerText += "0";
+    }
+});
+
+//add operator event listeners
+operatorAddition.addEventListener("click", () => {
+    if (!operatorInitialized) {
+        firstNumber = parseFloat(display.innerText);
+        display.innerHTML = initial;
+        initialized = false;
+        addition = true;
+        operatorInitialized = true;
+    } else {
+        //if operator is already initialized, solution from last input mus become firstnumber
+        //will have to come back to this part
+        secondNumber = parseFloat(display.innerText);
+        evaluate(firstNumber, secondNumber);
+        firstNumber = parseFloat(display.innerText);
+        initialized = false;
+        addition = true;
+    }
+});
+
+operatorSubtraction.addEventListener("click", () => {
+    if (!operatorInitialized) {
+        firstNumber = parseFloat(display.innerText);
+        display.innerHTML = initial;
+        initialized = false;
+        subtraction = true;
+        operatorInitialized = true;
+    } else {
+        //if operator is already initialized, solution from last input mus become firstnumber
+        //will have to come back to this part
+        secondNumber = parseFloat(display.innerText);
+        evaluate(firstNumber, secondNumber);
+        firstNumber = parseFloat(display.innerText);
+        initialized = false;
+        subtraction = true;
+    }
+});
+
+operatorMultiplication.addEventListener("click", () => {
+    if (!operatorInitialized) {
+        firstNumber = parseFloat(display.innerText);
+        display.innerHTML = initial;
+        initialized = false;
+        multiplication = true;
+        operatorInitialized = true;
+    } else {
+        //if operator is already initialized, solution from last input mus become firstnumber
+        //will have to come back to this part
+        secondNumber = parseFloat(display.innerText);
+        evaluate(firstNumber, secondNumber);
+        firstNumber = parseFloat(display.innerText);
+        initialized = false;
+        multiplication = true;
+    }
+});
+
+operatorDivision.addEventListener("click", () => {
+    if (!operatorInitialized) {
+        firstNumber = parseFloat(display.innerText);
+        display.innerHTML = initial;
+        initialized = false;
+        division = true;
+        operatorInitialized = true;
+    } else {
+        //if operator is already initialized, solution from last input mus become firstnumber
+        //will have to come back to this part
+        secondNumber = parseFloat(display.innerText);
+        evaluate(firstNumber, secondNumber);
+        firstNumber = parseFloat(display.innerText);
+        initialized = false;
+        division = true;
+    }
+});
+
+function evaluate(a, b) {
+    console.log("evaluate");
+    switch (true) {
+        case addition:
+            display.innerHTML = firstNumber + secondNumber;
+            console.log(firstNumber + secondNumber);
+            addition = false;
+            break;
+        case subtraction:
+            display.innerHTML = firstNumber - secondNumber;
+            subtraction = false;
+            break;
+        case multiplication:
+            display.innerHTML = firstNumber * secondNumber;
+            multiplication = false;
+            break;
+        case division:
+            display.innerHTML = firstNumber / secondNumber;
+            division = false;
+            break;
+    }   
+}
